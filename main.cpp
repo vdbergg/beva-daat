44750f68770c6fea0ccbc8bab478375fd168f0ec
Berg Gama <berg@meliuz.com.br>
2019-01-15 20:03:11 -0400

    Adiciona leitura de dados de arquivo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 #include "Trie.h"
 #include "EditDistance.h"
 
 using namespace std;
 
+vector<string> records;
+vector<string> queries;
+
+void readData(string& filename, vector<string>& recs) {
+    string str;
+    ifstream input(filename, ios::in);
+    while (getline(input, str)) {
+        for (auto i = 0; i < str.length(); i++) {
+            str[i] = tolower(str[i]);
+        }
+        recs.push_back(str);
+    }
+}
+
 int main() {
+    string datasetFile = "/home/vdberg/Mestrado/beva/dataset/aol/aol.txt";
+    string queryFile = "/home/vdberg/Mestrado/beva/dataset/aol/aol_queries/q7.txt";
+
+//    readData(datasetFile, records);
+//    readData(queryFile, queries);
+
     Trie* trie = new Trie();
 
-    string query;
+    string query; // = "battleships us 2";
 
     string keys[10] = {"abelha", "abacaxi", "abacate", "abobora", "arvore", "arco", "ave", "agil", "atrevido", "atraente"};
 
