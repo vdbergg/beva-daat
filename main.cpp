3ad51c1a4cdd99873fac12756631d551b751745d
Anderson Pimentel <andbrain@gmail.com>
2019-03-21 13:47:50 -0400

    chore: arg of query number

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int editDistanceThreshold;
-int algorithmType;
+int qryNumber;
 int datasetNumber;
 int sizeType;
 
@@ -16,12 +16,24 @@ void checkParameters(int argc, char** argv);
 
 int main(int argc, char** argv) {
     checkParameters(argc,argv);
+    string q, query, queryRemaining;
     Framework* framework = new Framework(editDistanceThreshold, datasetNumber, sizeType);
+    int indexMin, indexMax;
+    int algorithmType = 0; // = -> BEVA
 
-    for (string q : framework->queries) {
+    if(qryNumber == -1){
+        indexMin = 0;
+        indexMax = framework->queries.size();
+    }
+    else{
+        indexMin = qryNumber;
+        indexMax = qryNumber + 1;
+    }
 
-        string query = q.substr(0, 1);
-        string queryRemaining = q.substr(1);
+    for (int i = indexMin; i < indexMax; ++i) {
+        q = framework->queries[i];
+        query = q.substr(0, 1);
+        queryRemaining = q.substr(1);
 
         int count = 0;
 
@@ -42,22 +54,22 @@ void checkParameters(int number, char** values) {
 
     if (number == 4) {
         editDistanceThreshold = atoi(values[1]);
-        algorithmType = atoi(values[2]);
+        qryNumber = atoi(values[2]);
         datasetNumber = atoi(values[3]);
         sizeType = 3;
     }
     else if (number == 5) {
         editDistanceThreshold = atoi(values[1]);
-        algorithmType = atoi(values[2]);
+        qryNumber = atoi(values[2]);
         datasetNumber = atoi(values[3]);
         sizeType      = atoi(values[4]);
     }
     else {
         std::cout << "[ERROR] Missing parameters" << std::endl << std::endl;
-        std::cout << "./beva EDIT_DIST ALG_TYPE DATASET [SIZE_TYPE]" << std::endl;
+        std::cout << "./beva EDIT_DIST QRY_NUMBER DATASET [SIZE_TYPE]" << std::endl;
         std::cout << "EDIT_DIST -> edit distance number." << std::endl;
-        std::cout << "ALG_TYPE -> ." << std::endl;
-        std::cout << "\t BEVA -> 0" << std::endl;
+        std::cout << "QRY_NUMBER -> query number starting from 0" << std::endl;
+        std::cout << "\t ALL -> -1" << std::endl;
         std::cout << "DATASET -> dataset number." << std::endl;
         std::cout << "\t AOL -> 0" << std::endl;
         std::cout << "\t MEDLINE -> 1" << std::endl;
